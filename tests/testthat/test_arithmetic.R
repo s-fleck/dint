1fe691707e5031fa348812d25a32e8698052d9b5
Stefan Fleck <stefan.b.fleck@gmail.com>
2018-09-07 17:52:36 +0200

    added tests for seq functions

--- tests/testthat/test_arithmetic.R
+++ tests/testthat/test_arithmetic.R
@@ -2,6 +2,44 @@ context("arithmetic")
 
 
 
+# seq ---------------------------------------------------------------------
+
+test_that("seq.date_yw works", {
+  x <- seq(date_yw(2017, 1), date_yw(2019, 1))
+
+  expect_identical(
+    as.integer(x),
+    c(201701:201752, 201801:201853, 201901)
+  )
+})
+
+
+
+
+test_that("seq.date_ym works", {
+  x <- seq(date_ym(2017, 1), date_ym(2019, 1))
+
+  expect_identical(
+    as.integer(x),
+    as.integer(c(201701:201712, 201801:201812, 201901))
+  )
+})
+
+
+
+
+test_that("seq.date_ym works", {
+  x <- seq(date_yq(2017, 1), date_yq(2019, 1))
+
+  expect_identical(
+    as.integer(x),
+    as.integer(c(20171:20174, 20181:20184, 20191))
+  )
+})
+
+
+
+
 # y+ ----------------------------------------------------------------------
 
 test_that("y+.date_ym works", {
