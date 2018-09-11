d4627df5f12ff102aff55fea564a2e6f18d977e0
Stefan Fleck <stefan.b.fleck@gmail.com>
2018-09-11 10:18:53 +0200

    added assertions and removed parse_seq_by again

--- R/seq.R
+++ R/seq.R
@@ -15,7 +15,8 @@ seq.date_yw <- function(
 #' @rdname date_xx_arithmetic
 #' @export
 seq.date_yq <- function(from, to, ...){
-  res <- seq.int(as.integer(from), as.integer(to))
+  assert(is_date_yq(to))
+  res <- seq.int(as.integer(from), as.integer(to), by = by)
   as_date_yq(res[(res %% 10L) %in% 1:4])
 }
 
@@ -25,63 +26,7 @@ seq.date_yq <- function(from, to, ...){
 #' @rdname date_xx_arithmetic
 #' @export
 seq.date_ym <- function(from, to, ...){
+  assert(is_date_ym(to))
   res <- seq.int(as.integer(from), as.integer(to))
   as_date_ym(res[(res %% 100) %in% 1:12])
-}
-
-
-
-
-# parse by ----------------------------------------------------------------
-
-parse_seq_by <- function(
-  by
-){
-  assert(
-    is_scalar(by),
-    "'by' must be a scalar (a vector of length 1)"
-  )
-
-  if (is.integer(by))
-    return (by)
-
-  if (is.numeric(by)){
-    ret <- as.integer(by)
-    if (ret == by)
-      return(ret)
-    else
-      stop("If 'by' is a number, it must be an integer")
-  }
-
-
-  if (is.character(by)){
-    by2 <- strsplit(by, " ", fixed = TRUE)[[1L]]
-
-    if (identical(length(by2), 1L)){
-      ret <- 1L
-      unit <- by2
-    } else {
-      ret <- as.integer(by2[[1]])
-      assert(
-        identical(ret == by2[[1]], TRUE),
-        "If 'by' is a number, it must be an integer"
-      )
-      unit <- by2[[2]]
-    }
-
-    unit <- gsub("s$", "", unit)
-    valid_units <- c("year", "quarter", "month", "week", "isoyear")
-    assert(
-      unit %in% valid_units,
-      msg = sprintf(
-        "'%s' is not a valid unit. Valid units are: %s (optionally followed by an 's')",
-        unit, paste(sprintf("'%s'", valid_units), collapse = ", ")
-      )
-    )
-
-    return(structure(
-      ret,
-      unit = unit
-    ))
-  }
 }
